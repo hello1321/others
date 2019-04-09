577e55bc522c75a46f8b7fecb09bae24e279ce31
hello1321 <yhuseynov6@gmail.com>
2019-04-09 16:17:24 +0500

    others

---
+++ 8/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int bnums[10];
+
+    int LuckyNums[20]={4,7,45,23,11};
+    LuckyNums[0]=10;
+    cout<< LuckyNums[0];
+
+    return 0;
+}
---
+++ 9/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+void sayHi(string name){
+
+    cout<<"Hello"<<name ;
+
+
+}
+
+int main()
+{
+    cout<<"Top";
+    sayHi("Mike");
+    cout<<"Hi User";
+    return 0;
+}
